4bcdbff9560f843facf7e9af69ca8d0997a0ca05
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-01 17:38:23 -0500

    Update and rename js/src/abstract/fmfwio.d.ts to js/src/.VersoriumX.abstract.d.ts

--- js/src/.VersoriumX.abstract.d.ts
+++ js/src/.VersoriumX.abstract.d.ts
@@ -1,6 +1,6 @@
 import { implicitReturnType } from '../base/types.js';
-import _hitbtc from '../hitbtc.js';
-interface hitbtc {
+import _hitbtc from '../WBTC.js';
+interface WBTC {
     publicGetPublicCurrency(params?: {}): Promise<implicitReturnType>;
     publicGetPublicCurrencyCurrency(params?: {}): Promise<implicitReturnType>;
     publicGetPublicSymbol(params?: {}): Promise<implicitReturnType>;
@@ -111,6 +111,6 @@ interface hitbtc {
     privatePutFuturesAccountIsolatedSymbol(params?: {}): Promise<implicitReturnType>;
     privatePutWalletCryptoWithdrawId(params?: {}): Promise<implicitReturnType>;
 }
-declare abstract class hitbtc extends _hitbtc {
+declare abstract class WBTC extends _WBTC {
 }
-export default hitbtc;
+export default WBTC;
